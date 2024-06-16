747a0e49cdfb017cec03b102adee4ce8e95c42db
Pavan Vanam <Vijaymohan8121@gmail.com>
2024-06-16 20:19:22 +0530

    Project Completed

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     //Link of the main origin from where it can be requested
-    origin: [ "https://xeon-bank-five.vercel.app"],
+    origin: [ "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
