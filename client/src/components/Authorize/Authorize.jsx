ff23a2bb75c33f3e179513033203f7ba0f940638
Pavan Vanam <Vijaymohan8121@gmail.com>
2024-06-17 21:51:16 +0530

    First commit Intilize the Project

--- client/src/components/Authorize/Authorize.jsx
+++ client/src/components/Authorize/Authorize.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Sign_IN from "./Sign_IN";
-import image from '../../icons/auth.jpg';
-import SignUp from './Sign_UP';
+import image from "../../icons/auth.jpg";
+import SignUp from "./Sign_UP";
 
 const Authorize = () => {
   const [flag, setFlag] = useState(false);
@@ -14,21 +14,27 @@ const Authorize = () => {
     <section className="flex flex-col md:flex-row h-screen">
       <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-white p-8 relative">
         {flag ? (
-           <div className='flex flex-col gap-1'>
+          <div className="flex flex-col gap-1">
             <SignUp onSignupSuccess={handleToggle} />
             <p className="text-center text-gray-600 font-bold">
               Already have an account?{" "}
-              <button onClick={handleToggle} className="text-blue-500 hover:underline">
+              <button
+                onClick={handleToggle}
+                className="text-blue-500 hover:underline"
+              >
                 Login
               </button>
             </p>
           </div>
-        ):(
-          <div className='flex flex-col gap-1'>
+        ) : (
+          <div className="flex flex-col gap-1">
             <Sign_IN />
             <p className="text-center text-gray-600">
               Don't have an account?{" "}
-              <button onClick={handleToggle} className="text-blue-500 hover:underline">
+              <button
+                onClick={handleToggle}
+                className="text-blue-500 hover:underline"
+              >
                 Sign Up
               </button>
             </p>
@@ -37,10 +43,10 @@ const Authorize = () => {
       </div>
       {/* Hide image on small devices */}
       <div className="hidden md:block  items-center justify-center bg-gray-100">
-        <img src={image} className="w-full h-full object-cover" alt="Auth" />
+        <img src="/pic.png" className="w-full h-full object-cover" alt="Auth" />
       </div>
     </section>
   );
-}
+};
 
 export default Authorize;
